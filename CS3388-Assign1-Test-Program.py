689f28470d18b74fa0e4a26a8606e074ebe0526e
Jason Chin <jason.rob.chin@gmail.com>
2020-01-17 15:36:56 -0500

    finish

--- CS3388-Assign1-Test-Program.py
+++ CS3388-Assign1-Test-Program.py
@@ -15,6 +15,6 @@ while t < 2.0*pi:
     t += dt
     x2 = 256 + int(100.0*(1.5*cos(t) - cos(13.0*t)))
     y2 = 256 + int(100.0*(1.5*sin(t) - sin(13.0*t)))
-    window.drawLine((x1,y1),(x2,y2),color)
+    window.drawLine((x1,y1),(x2,y2), color)
 
 window.saveImage("testImage.png")
--- graphicsWindow.py
+++ graphicsWindow.py
@@ -1,9 +1,12 @@
 import logging
 import math
+import random
 
 from PIL import Image
 
 class graphicsWindow:
+
+    sign = lambda _, x: x and (1, -1)[x<0]
 
     def __init__(self,width=640,height=480):
         self.__mode = 'RGB'
@@ -25,172 +28,26 @@ class graphicsWindow:
         self.__canvas.save(fileName)
     
     def drawLine(self,p1,p2,color):
-        """
-        Pseudo code:
-        begin
-            plot (x1, y1)
-            for (i= x1 to x2 by step of 1) {
-                if (i== x1 ) {
-                    pi=2Δy−Δx
-                }
-                else {
-                    if ( pi<0 ) {
-                        pi= pi+2 Δ y
-                    }
-                    else {
-                        pi= pi+2 Δ y−2Δ x
-                        y1++
-                    }
-                    x1++
-                    plot (x1, y1)
-                }
-            }
-        end
-        """
-        plot = lambda x, y, c=color: self.drawPixel((x, y), c)
-        sign = lambda x: x and (1, -1)[x<0]
-
-        x1, y1 = p1
-        x2, y2 = p2
-        dx = abs(x2 - x1)
-        dy = abs(y2 - y1)
-        dxSign = sign(x2 - x1)
-        dySign = sign(y2 - y1)
-        try:
-            m = dy*dySign / dx*dxSign
-        except ZeroDivisionError:
-            m = None
+        x1, y1, x2, y2 = *p1, *p2
+        dx, dy = abs(x2 - x1), abs(y2 - y1)
+        sX, sY = self.sign(x2 - x1), self.sign(y2 - y1)
 
         if dx >= dy:
-            d1, y, ySign = dy*dySign, y1, dySign
-            d2, x, xSign = dx*dxSign, x1, dxSign
+            x, xi, y, yi, d1, d2 = x1, sX, y1, sY, dy, dx
+            plot = lambda x, y, c=color: self.drawPixel((x, y), c)
+        elif dy > dx:
+            x, xi, y, yi, d1, d2 = y1, sY, x1, sX, dx, dy
+            plot = lambda x, y, c=color: self.drawPixel((y, x), c)
         else:
-            d1, y, ySign = dx*dxSign, x1, dxSign
-            d2, x, xSign = dy*dySign, y1, dySign
-
-        c = (255, 0, 0)
-        plot(x, y, c)
+            raise Exception(f"{p1} to {p2} is not drawn")
+        
+        plot(x, y)
         pi = 2 * d1 - d2
-        for i in range(abs(d2)):
+        for i in range(d2):
             if pi < 0:
                 pi = pi + 2 * d1
             else:
                 pi = pi + 2 * d1 - 2 * d2
-                y += ySign
-            x += xSign
-            if x >= 512 or y >= 512:
-                breakpoint()
-                pass
-            else:
-                plot(x, y, c)
-
-        # if m is not None:
-        #     if x1 < x2 and y1 < y2 and 0 <= m and m <= 1: # 1st octant
-        #         c = (255, 0, 0)
-        #         plot(x1, y1, c)
-        #         pi = 2 * dy - dx
-        #         for i in range(abs(dx)):
-        #             if pi < 0:
-        #                 pi = pi + 2 * dy
-        #             else:
-        #                 pi = pi + 2 * dy - 2 * dx
-        #                 y1 += 1
-        #             x1 += 1
-        #             plot(x1, y1, c)
-        #     elif x1 < x2 and y1 < y2 and 1 < m and m < math.inf: # 2nd octant
-        #         c = (0, 255, 0)
-        #         plot(x1, y1, c)
-        #         pi = 2 * dx - dy
-        #         for i in range(abs(dy)):
-        #             if pi < 0:
-        #                 pi = pi + 2 * dx
-        #             else:
-        #                 pi = pi + 2 * dx - 2 * dy
-        #                 x1 += 1
-        #             y1 += 1
-        #             plot(x1, y1, c)
-        #     elif x1 > x2 and y1 < y2 and -1 > m and m > -math.inf: # 3rd octant
-        #         c = (0, 0, 255)
-        #         plot(x1, y1, c)
-        #         pi = 2 * -dx - dy
-        #         for i in range(abs(dy)):
-        #             if pi < 0:
-        #                 pi = pi + 2 * -dx
-        #             else:
-        #                 pi = pi + 2 * -dx - 2 * dy
-        #                 x1 -= 1
-        #             y1 += 1
-        #             plot(x1, y1, c)
-        #     elif x1 > x2 and y1 < y2 and 0 >= m and m >= -1: # 4th octant
-        #         c = (255, 255, 0)
-        #         plot(x1, y1, c)
-        #         pi = 2 * dy - -dx
-        #         for i in range(abs(dx)):
-        #             if pi < 0:
-        #                 pi = pi + 2 * dy
-        #             else:
-        #                 pi = pi + 2 * dy - 2 * -dx
-        #                 y1 += 1
-        #             x1 -= 1
-        #             plot(x1, y1, c)
-        #     elif x1 > x2 and y1 > y2 and 0 < m and m <= 1: # 5th octant
-        #         c = (65, 0, 165)
-        #         plot(x1, y1, c)
-        #         pi = 2 * -dy - -dx
-        #         for i in range(abs(dx)):
-        #             if pi < 0:
-        #                 pi = pi + 2 * -dy
-        #             else:
-        #                 pi = pi + 2 * -dy - 2 * -dx
-        #                 y1 -= 1
-        #             x1 -= 1
-        #             plot(x1, y1, c)
-        #     elif x1 > x2 and y1 > y2 and 1 < m and m < math.inf: # 6th octant
-        #         # TODO: check if this is correct
-        #         c = (255, 165, 255)
-        #         plot(x1, y1, c)
-        #         pi = 2 * -dx - -dy
-        #         for i in range(abs(-dy)):
-        #             if pi < 0:
-        #                 pi = pi + 2 * -dx
-        #             else:
-        #                 pi = pi + 2 * -dx - 2 * -dy
-        #                 x1 -= 1
-        #             y1 -= 1
-        #             plot(x1, y1, c)
-        #     elif x1 < x2 and y1 > y2 and -1 > m and m > -math.inf: # 7th octant
-        #         # TODO: check if this is correct
-        #         c = (255, 165, 65)
-        #         plot(x1, y1, c)
-        #         pi = 2 * dx - -dy
-        #         for i in range(abs(-dy)):
-        #             if pi < 0:
-        #                 pi = pi + 2 * dx
-        #             else:
-        #                 pi = pi + 2 * dx - 2 * -dy
-        #                 x1 += 1
-        #             y1 -= 1
-        #             plot(x1, y1, c)
-        #     elif x1 < x2 and y1 > y2 and 0 > m and m >= -1: # 8th octant
-        #         c = (0, 255, 255)
-        #         plot(x1, y1, c)
-        #         pi = 2 * -dy - dx
-        #         for i in range(abs(dx)):
-        #             if pi < 0:
-        #                 pi = pi + 2 * -dy
-        #             else:
-        #                 pi = pi + 2 * -dy - 2 * dx
-        #                 y1 -= 1
-        #             x1 += 1
-        #             plot(x1, y1, c)
-        #     else:
-        #         x = min(x1, x2)
-        #         for i in range(abs(dx)):
-        #             plot(x+i, y1, (128, 0, 128))
-        #         pass
-        # elif dx == 0: # vertical line
-        #     y = min(y1, y2)
-        #     for i in range(abs(dy)):
-        #         plot(x1, y+i, (128, 0, 128))
-        # else:
-        #     raise Exception(f"{p1} to {p2} not drawn")
+                y += yi
+            x += xi
+            plot(x, y)
